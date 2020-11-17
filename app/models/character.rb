6e10c928f7190c35c4752bd345c1c56c00b685ef
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-17 01:14:09 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -4,6 +4,6 @@ class Character < ActiveRecord::Base
   belongs_to :show
 
   def say_that_thing_you_say
-    "#{self.first_name}: #{self.catchphrase}"
+    "#{self.name}: #{self.catchphrase}"
   end
 end
