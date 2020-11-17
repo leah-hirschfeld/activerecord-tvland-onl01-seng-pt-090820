d557ae5bbcec78aebf8dbf99098529a9ce882a2b
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-17 01:55:30 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,11 @@
+require 'pry'
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actor, through: :characters
+
+  def actors_list
+    actor_array = []
+    actor_array << "#{self.actor[0].first_name} #{self.actor[0].last_name}"
+  end
 end
