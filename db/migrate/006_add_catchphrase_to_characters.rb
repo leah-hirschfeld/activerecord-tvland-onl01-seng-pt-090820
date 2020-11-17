ede27a619c8dc578e836b8ba14d516df00b9a3a1
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-17 01:16:17 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_catchphrase_to_characters.rb
+++ db/migrate/006_add_catchphrase_to_characters.rb
@@ -1,5 +1,5 @@
 class AddCatchphraseToCharacter < ActiveRecord::Migration[5.1]
   def change
-    add_column :catchprase, :characters, :string
+    add_column :characters, :catchprase, :string
   end
 end
