8dc6f810afbe502557955fa89e4717f67aeb6816
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-17 01:27:36 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,6 +5,7 @@ class Show < ActiveRecord::Base
   has_many :actor, through: :characters
 
   def actors_list
+    actor_array = []
     "#{self.actor[0].first_name} #{self.actor[0].last_name}"
   end
 end
