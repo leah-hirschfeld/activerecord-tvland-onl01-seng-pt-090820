c71bb374c3aa44be833634003b0492fb1c7760e6
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-17 00:53:48 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -2,7 +2,8 @@ class CreateCharacters < ActiveRecord::Migration[5.1]
   def change
     create_table :characters do |t|
       t.string :name
-      t.string :last_name
+      t.integer :actor_id
+      t.integer :show_id
     end
   end
 end
