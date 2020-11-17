75b1b4cf66b55f89bc5268f6f2ecf8fd2ba12ac8
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-17 01:12:50 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,7 +2,7 @@ require 'pry'
 class Character < ActiveRecord::Base
   belongs_to :actor
   belongs_to :show
-
+binding.pry
   def say_that_thing_you_say
     binding.pry
     "#{self.first_name}: #{self.catchphrase}"
