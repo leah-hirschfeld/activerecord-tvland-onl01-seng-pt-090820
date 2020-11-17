cec668793efd31e50dddbce8fd438a4c374ea542
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-17 00:51:54 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,6 +1,6 @@
 class CreateActors < ActiveRecord::Migration[5.1]
   def change
-    create_table :networks do |t| 
+    create_table :networks do |t|
       t.string :call_letters
       t.integer :channel
     end
