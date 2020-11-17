f6bbea997203117abb9f81afff1a508b47774356
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-17 01:18:45 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_day_to_shows.rb
+++ db/migrate/007_add_day_to_shows.rb
@@ -1,5 +1,7 @@
 class AddDayToShows < ActiveRecord::Migration[5.1]
   def change
     add_column :day, :shows, :string
+    add_column :day, :shows, :string
+    add_column :day, :shows, :string
   end
 end
