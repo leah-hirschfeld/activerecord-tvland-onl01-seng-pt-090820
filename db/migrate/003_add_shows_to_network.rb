4174ffc9b71f95ca97d04df2ae406b9f2c977947
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-17 00:55:30 +0000

    Automatically backed up by Learn

--- db/migrate/003_add_shows_to_network.rb
+++ db/migrate/003_add_shows_to_network.rb
@@ -1,5 +1,5 @@
-class AddCatchphraseToCharacters < ActiveRecord::Migration[5.1]
+class AddShowsToNetwork < ActiveRecord::Migration[5.1]
   def change
-    add_column :characters, :catchphrase :text
+    add_column :shows, :network_id, :integer
   end
 end
